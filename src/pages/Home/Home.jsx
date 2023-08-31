e44dfcd3cf1ed6fc6ac05923fcca657231283e67
millenialia <lenalebo@gmail.com>
2023-08-31 08:41:15 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,11 @@
-// import ContactsIcon from '@mui/icons-material/Contacts';
 import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 import { ContactsIcon } from 'components/ContactsIcon/ContactsIcon';
 import css from "./Home.module.css"
-import Typography from '@mui/material/Typography';
 
 const Home = () => {
   return (
     <Container component="main" className={css.homeBox}>
-
         <ContactsIcon/>
       <Typography component="h1" variant="h2" className={css.header}>Everything you need to manage your contacts.</Typography>
     </Container>
