4e5420cec9a77d9736b9e9a5d2a65336ba4fa6c8
millenialia <lenalebo@gmail.com>
2023-08-31 08:40:44 +0200

    svg home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
-import ContactsIcon from '@mui/icons-material/Contacts';
+// import ContactsIcon from '@mui/icons-material/Contacts';
 import Container from '@mui/material/Container';
+import { ContactsIcon } from 'components/ContactsIcon/ContactsIcon';
 import css from "./Home.module.css"
 import Typography from '@mui/material/Typography';
 
 const Home = () => {
   return (
     <Container component="main" className={css.homeBox}>
-        <ContactsIcon sx={{width:"100px", height: "auto"}} className={css.contactsIcon} />
+
+        <ContactsIcon/>
       <Typography component="h1" variant="h2" className={css.header}>Everything you need to manage your contacts.</Typography>
     </Container>
   )
